1f8affdb79c87e30da32f2ca2ffdf149a65a2401
Sanjay Kumar Saini <kumarsaini0098@gmail.com>
2024-10-20 06:04:21 +0000

    automate script

--- health.py
+++ health.py
@@ -1,30 +1,13 @@
-from fastapi import FastAPI
-import psutil
-import platform
-from datetime import datetime
+import time
+import requests
 
-app = FastAPI()
+url = "https://vyan-py.onrender.com/"  # Replace with your target URL
 
-def get_server_health():
-    # Get system information
-    system_info = {
-        "hostname": platform.node(),
-        "platform": platform.system(),
-        "platform_version": platform.version(),
-        "architecture": platform.machine(),
-        "cpu_count": psutil.cpu_count(logical=True),
-        "cpu_usage_percent": psutil.cpu_percent(interval=1),
-        "memory_total_gb": round(psutil.virtual_memory().total / (1024 ** 3), 2),
-        "memory_used_percent": psutil.virtual_memory().percent,
-        "disk_usage_percent": psutil.disk_usage('/').percent,
-        "boot_time": datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-    }
-    return system_info
-
-@app.get("/api/serverhealthcheck")
-def server_health_check():
-    health_info = get_server_health()
-    return health_info
-
-# Run the app
-# To run: `uvicorn <script_name>:app --reload`
+while True:
+    try:
+        response = requests.get(url)
+        print(f"Request sent to {url}. Response Code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    
+    time.sleep(300)  # Sleep for 5 minutes (300 seconds)
